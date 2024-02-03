0501029a305d26ad25ddb0ab4f411d1375ce7997
Laakheey <sunilpandey0086@gmail.com>
2024-02-03 16:31:58 +0545

    node with build

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDb from "../utils/dbConnect.js";
-import wallpaper_router from '../router/wallpaper-route.js';
-import cors from 'cors'
-
+import connectDb from "../utils/dbConnect";
+import wallpaper_router from '../router/wallpaper-route';
+import cors from 'cors';
 dotenv.config();
-
 const app = express();
-
 app.use(express.json());
-
 app.use(cors());
-
 app.use("/api/wallpaper", wallpaper_router);
-
 connectDb()
-  .then(() => {
+    .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log(`Listening to port ${process.env.PORT}`);
+        console.log(`Listening to port ${process.env.PORT}`);
     });
-  })
-  .catch(() => {
+})
+    .catch(() => {
     console.log("Internet connection problem");
-  });
+});
+//# sourceMappingURL=index.js.map
